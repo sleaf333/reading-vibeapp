6fd41ffef3658179c5c71dbbe97dbf1675577908
sleaf333 <samleaf@gmail.com>
2026-06-10 10:01:52 -0500

    Remove Wemo app (moved to wemo-switchfix repo)

--- wemo-windows/Remove Matter Bridge.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem Stops and removes the Google Home bridge background task.
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0matter-bridge\MatterBridgeTask.ps1" remove
-pause

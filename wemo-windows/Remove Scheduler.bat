a71edc6f04320ead90251ce8dc5cb373787ec4b4
sleaf333 <samleaf@gmail.com>
2026-06-10 10:01:45 -0500

    Remove Wemo app (moved to wemo-switchfix repo)

--- wemo-windows/Remove Scheduler.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem Removes the dusk/dawn background task.
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ". '%~dp0WemoLib.ps1'; Remove-WemoSchedulerTask; Write-Host 'Scheduler removed.'"
-pause
